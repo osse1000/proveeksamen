ba1697a6b27abcf30e8fc87f72ef5f825031d0ab
osse1000 <osse1000@stud.ek.dk>
2026-04-28 10:26:41 +0200

    uvb

--- src/song.java
+++ src/song.java
@@ -1,7 +1,14 @@
 public class song {
 
-private String title;
-private String artist;
-private int duration;
+    private String title;
+    private String artist;
+    private int duration;
+
+
+    public song(String title, String artist, int duration) {
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+    }
 
 }
